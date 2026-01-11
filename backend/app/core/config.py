dbfa330ee9b900f69911d2bad61ac5d51c3215a3
Timo Surmann <github@timosur.com>
2026-01-11 09:17:05 +0100

    fix(backend): config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -17,18 +17,26 @@ class Settings(BaseSettings):
     "http://localhost:8000",
     "http://localhost:3000",
     "https://vinyl.docker.local",
+    "https://vinyl.home.timosur.com",
   ]
 
   # The following variables need to be defined in environment
   DATABASE_URL: PostgresDsn
   ASYNC_DATABASE_URL: Optional[PostgresDsn]
 
-  @validator("ASYNC_DATABASE_URL")
+  @validator("ASYNC_DATABASE_URL", pre=True, always=True)
   def build_async_database_url(cls, v: Optional[str], values: Dict[str, Any]):
     """Builds ASYNC_DATABASE_URL from DATABASE_URL."""
-    v = values["DATABASE_URL"]
-    # Replace postgresql:// with postgresql+asyncpg://
-    return v.replace("postgresql", "postgresql+asyncpg", 1) if v else v
+    if v:
+      return v
+    database_url = values.get("DATABASE_URL")
+    if database_url:
+      # Replace postgresql:// with postgresql+asyncpg://
+      return database_url.replace("postgresql", "postgresql+asyncpg", 1)
+    return v
+
+  class Config:
+    case_sensitive = True
 
 
 settings = Settings()
